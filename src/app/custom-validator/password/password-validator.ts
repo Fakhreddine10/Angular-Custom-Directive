2223b046101123cf515e491d402ea90ecc0031d8
Fakhreddine10 <fakhreddine.belgaied@gmail.com>
2017-12-07 23:05:41 +0100

    bug fixing

--- src/app/custom-validator/password/password-validator.ts
+++ src/app/custom-validator/password/password-validator.ts
@@ -3,8 +3,8 @@ import { AbstractControl, Validators, ValidatorFn } from '@angular/forms';
 
 export const passwordval = (regexval: string): ValidatorFn => {
   return (control: AbstractControl): {[key: string]: boolean} => {
-      console.log(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/.test(control.value));
-      let match:boolean=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/.test(control.value);
+       let regex:RegExp=new RegExp(regexval);
+       let match:boolean=regex.test(control.value);
       if(match) return {passwordval:true};
       return null;
   };
