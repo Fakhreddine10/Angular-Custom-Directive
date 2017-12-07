4943fb921f75a1dfb3cf483c985a6d855a81772e
Fakhreddine10 <fakhreddine.belgaied@gmail.com>
2017-12-08 00:56:27 +0100

    add custom email validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CustomValidationAndFormattingModule } from "./custom-validation-and-formatting/custom-validation-and-formatting.module";
 import { FormsModule } from "@angular/forms";
+import { EmailValidatorDirective } from './custom-validator/email-validator.directive';
 
 @NgModule({
   declarations: [
-    AppComponent, 
+    AppComponent,
+    EmailValidatorDirective, 
   ],
   imports: [
     BrowserModule,
