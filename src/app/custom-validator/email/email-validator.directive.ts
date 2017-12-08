be5f746bf955f0af07c2f5dd69e023c6bfd341f6
Fakhreddine10 <fakhreddine.belgaied@gmail.com>
2017-12-08 01:20:35 +0100

    add custom email validation with prefix

--- src/app/custom-validator/email/email-validator.directive.ts
+++ src/app/custom-validator/email/email-validator.directive.ts
@@ -1,10 +1,42 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, forwardRef, OnInit, SimpleChanges, OnChanges } from '@angular/core';
+import { NG_VALIDATORS, Validator, ValidatorFn, AbstractControl,ValidationErrors } from '@angular/forms';
+import { emailval } from "./email.validator";
 
+const EMAIL_VALIDATOR: any = {
+  provide: NG_VALIDATORS,
+  useExisting: forwardRef(() => EmailValidator),
+  multi: true
+};
 @Directive({
-  selector: '[appEmailValidator]'
+  selector: '[emailvalidator][formControlName],[emailvalidator][formControl],[emailvalidator][ngModel]',
+  providers: [EMAIL_VALIDATOR]
 })
-export class EmailValidatorDirective {
+export class EmailValidator implements OnInit,Validator,OnChanges {
+  @Input('emailvalidator') prefix:string;
 
   constructor() { }
+
+ private validator: ValidatorFn;
+ private onChange: () => void;
+
+  ngOnInit(): void {
+    this.validator=emailval(this.prefix);
+    
+  }
+   ngOnChanges(changes: SimpleChanges): void {
+    for (let key in changes) {
+      if (key === 'emailvalidator') {
+        this.validator = emailval(changes[key].currentValue);
+        if (this.onChange) this.onChange();
+      }
+    }
+  }
+  validate(c: AbstractControl): ValidationErrors {
+    return this.validator(c);
+  }
+  registerOnValidatorChange(fn: () => void): void {
+    this.onChange=fn;
+  }
+  
 
 }
